aa75c233dfc7a6323564e60f810a11d03f010e92
Tomasz Żurawski <mgrtomaszzurawski@gmail.com>
2020-02-16 10:42:00 +0100

    test2

---
+++ src/main/java/pl/barksville/barksville/spring/model/entities/user/Test.java
@@ -0,0 +1,6 @@
+package pl.barksville.barksville.spring.model.entities.user;
+
+import pl.barksville.barksville.spring.model.entities.base.ParentEntity;
+
+public class Test  extends ParentEntity {
+}
